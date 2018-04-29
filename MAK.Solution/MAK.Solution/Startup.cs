a4e3b2ea09317628f1f4c843c75b38fc5b1af1cf
Sumit Saha <sumit87.dgp@gmail.com>
2018-04-30 05:03:16 +0530

    Inital Commit Testing

--- MAK.Solution/MAK.Solution/Startup.cs
+++ MAK.Solution/MAK.Solution/Startup.cs
@@ -27,7 +27,7 @@ namespace MAK.Solution
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Welcome to Maitra Associate Kitchen");
             });
         }
     }
